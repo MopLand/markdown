a61aadfed3122429559c17f6ffee3cec78f3eb0d
王磊 <veryide@qq.com>
2018-10-12 00:40:05 +0800

    更新文档；

--- index.php
+++ index.php
@@ -5,107 +5,129 @@
 	<title>Markdown Reading</title>
 	<meta name="renderer" content="webkit">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
-	<script src="//cdn.bootcss.com/vue/2.3.4/vue.min.js"></script>
+	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css">
 	<style>
-	@media (min-width: 992px){
-		.open{
-			display: none;
+		address {
+			margin-bottom: 20px;
+			font-style: normal;
+			line-height: 1.42857143
 		}
-	}
-	@media (max-width: 992px){	
-		.open{
-			float: right;
-			font-size: 26px;
-			vertical-align: middle;
-			line-height: 28px;
-			margin: 8px 10px;			
-			text-decoration: none!important;
-			color: #444;
-			outline: none;
-		}	
-	}
+
+		code,kbd,pre,samp {
+			font-family: Menlo,Monaco,Consolas,"Courier New",monospace
+		}
+
+		code {
+			padding: 2px 4px;
+			font-size: 90%;
+			color: #c7254e;
+			background-color: #f9f2f4;
+			border-radius: 4px
+		}
+
+		kbd {
+			padding: 2px 4px;
+			font-size: 90%;
+			color: #fff;
+			background-color: #333;
+			border-radius: 3px;
+			-webkit-box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
+			box-shadow: inset 0 -1px 0 rgba(0,0,0,.25)
+		}
+
+		kbd kbd {
+			padding: 0;
+			font-size: 100%;
+			font-weight: 700;
+			-webkit-box-shadow: none;
+			box-shadow: none
+		}
+
+		pre {
+			display: block;
+			padding: 9.5px;
+			margin: 0 0 10px;
+			font-size: 0.85rem;
+			line-height: 1.42857143;
+			color: #333;
+			word-break: break-all;
+			word-wrap: break-word;
+			background-color: #f5f5f5;
+			border: 1px solid #ccc;
+			border-radius: 4px
+		}
+
+		pre code {
+			padding: 0;
+			font-size: inherit;
+			color: inherit;
+			white-space: pre-wrap;
+			background-color: transparent;
+			border-radius: 0
+		}
+
 	</style>
 </head>
 <body>
 
 <?php
 $base = './../notes/';
-$list = glob( $base.'*.md' );
 $file = isset( $_GET['file'] ) ? $_GET['file'] : NULL;
 $local = $base . $file .'.md';
 ?>
 
+<nav aria-label="breadcrumb">
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="/">在线实用小工具</a></li>
+		<li class="breadcrumb-item active" aria-current="page"><?php echo $file .'.md';?></li>
+	</ol>
+</nav>
+
+<div class="my-3 mx-3"></div>
+
 <div class="container" id="app">
-
-	<div class="row">
-	
-	  <div class="col-md-2">	  
-		<div class="page-header">
-		  <a class="open" v-on:click="toggle()">&#9776;</a>
-		  <h1>Catalog</h1>
-		</div>		
-		<div class="list-group" v-if="status">
-		  <?php foreach ($list as $item) { ?>
-		  <a href="<?php echo basename($item);?>" class="list-group-item <?php echo basename($item, '.md') == $file ? 'active' : NULL;?>"><?php echo basename($item, '.md');?></a>
-		  <?php } ?>
-		</div>  
-	  </div>
-	  
-	  <div class="col-md-10">
-		  
-		<?php if( file_exists( $local ) ): ?>
-			
-			<div class="page-header">
-			  <h1><?php echo $file;?>.md</h1>
-			</div>
-			
-			<?php
-				require_once 'Parsedown.php';
-				$Parsedown = new Parsedown();
-				$source = file_get_contents( $local, FILE_USE_INCLUDE_PATH);
-				echo $Parsedown->text($source);
-			?>
-			
-			<hr />
-			
-			<div class="page-footer">
-			  <small class="pull-right">最后更新：<?php echo date('Y/m/d H:i:s', filemtime( $local ) );?></small>
-			</div>
-			
-		<?php else: ?>
-			
-			<div class="page-header">
-			  <h1>Markdown</h1>
-			</div>	  
-			<p>Stay Hungry. Stay Foolish.</p>
-			
-		<?php endif; ?>
-		 
-	  </div>
-	</div>
-</div>
-
-<script>	
-var catlog = new Vue({
-	el : '#app',
-	delimiters: ['${', '}'],
-	data : {
-		status : ( window.innerWidth >= 992 )
-	},
-	methods : {
-		toggle : function(){
-			this.status = 1 - this.status;
-			console.log( this.status );
-		}
+		
+	<?php
+	$list = glob( $base.'*.md' );
+	foreach( $list as $k => $v ){
+		echo '<a class="btn btn-sm m-1 '. ( basename($v, '.md') == $file ? 'btn-secondary' : 'btn-outline-secondary' ) .'" href="'. basename( $v ) .'" role="button">'. basename($v, '.md') .'</a>';
 	}
-});
-</script>
+	?>
+
+	<hr />
+
+	<?php if( file_exists( $local ) ): ?>
+		
+		<?php
+			require_once 'Parsedown.php';
+			$Parsedown = new Parsedown();
+			$source = file_get_contents( $local, FILE_USE_INCLUDE_PATH);
+			echo $Parsedown->text($source);
+		?>
+		
+		<hr />
+		
+		<div class="page-footer">
+			<small class="pull-right">最后更新：<?php echo date('Y/m/d H:i:s', filemtime( $local ) );?></small>
+		</div>
+		
+	<?php else: ?>
+		
+		<div class="page-header">
+			<h1>Markdown</h1>
+		</div>	  
+		<p>Stay Hungry. Stay Foolish.</p>
+		
+	<?php endif; ?>	
+
+</div>
 
 <hr />
 <footer>
 	<p class="text-center"><i>Powered by <a href="http://www.veryide.com/" target="_blank">VeryIDE</a></i></p>
 </footer>
+
+<script src="./project/app.js"></script>
 
 </body>
 </html>
